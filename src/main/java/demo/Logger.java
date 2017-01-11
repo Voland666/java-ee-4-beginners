4aaa8191a99c28d27a264fbedbbfd0485e1a2f44
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-01-11 16:22:57 +0300

    Add POJO demo.

--- src/main/java/demo/Logger.java
+++ src/main/java/demo/Logger.java
@@ -86,3 +86,30 @@ class Main {
         Logger.m(new Integer(5));
     }
 }
+
+//POJO
+class User {
+    private int id;
+    private String name;
+
+    User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
